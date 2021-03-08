0b2a94ef23656b119286eafd90cae90c3089ba8e
Mahmoud Samy <mahmoudsamy50@outlook.com>
2021-03-08 22:00:51 +0200

    add: added animations to the cover text

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-function updateCardsHeight() {
-    const querySelectorOfCoursesTitles = '.course-title h3'
-    const querySelectorOfInstructorsNames = '.course-info h4'
-
-    updateMaxHeightOfSimilarElements(querySelectorOfCoursesTitles)
-    updateMaxHeightOfSimilarElements(querySelectorOfInstructorsNames)
-}
+const coverElement = document.querySelector('.cover-text')
 
 function updateMaxHeightOfSimilarElements(querySelector) {
     const similarElements = [...document.querySelectorAll(querySelector)]
@@ -14,5 +8,22 @@ function updateMaxHeightOfSimilarElements(querySelector) {
     similarElements.forEach(element => element.style.height = maxHeight + 'px')
 }
 
+function updateCardsHeight() {
+    const querySelectorOfCoursesTitles = '.course-title h3'
+    const querySelectorOfInstructorsNames = '.course-info h4'
+
+    updateMaxHeightOfSimilarElements(querySelectorOfCoursesTitles)
+    updateMaxHeightOfSimilarElements(querySelectorOfInstructorsNames)
+}
+
 window.addEventListener('DOMContentLoaded', updateCardsHeight)
 window.addEventListener('resize', updateCardsHeight)
+
+
+document.addEventListener('mousemove', (event) => {
+    const mouseLocationX = event.clientX
+    const mouseLocationY = event.clientY
+
+    coverElement.style.top = mouseLocationX / 100 - mouseLocationY / 200 + 'px'
+    coverElement.style.left = mouseLocationY / 100 - mouseLocationX / 200 + 'px'
+})
